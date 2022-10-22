7db7cf4e4c511f1819a7e6730c4552f601b4492b
xuanquang1133 <93111059+xuanquang1133@users.noreply.github.com>
2022-10-22 22:58:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: ' Nói nghe nè Tư ',
+    introTitle: ' Nói nghe nè Vy ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
