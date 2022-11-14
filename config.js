d25f88eae891d569ed79c4983980818d53093954
xuanquang1133 <93111059+xuanquang1133@users.noreply.github.com>
2022-11-14 18:22:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: ' Nói nghe nè Nguyễn Hường ',
+    introTitle: ' Nói nghe nè Ngoc Mai ',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao mày cứ đi nhầm đường
+    Lạc vào tim tao lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Phải chăng mình đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích tớ <33',
+    btnNo: 'Không, không chịu à :3',
     question:'Mình không biết cách yêu ai đó đến suốt cả cuộc đời. Nhưng một vài khoảnh khắc, mình đã muốn cả đời chỉ yêu duy nhất một người thôi!',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho mình nè <3',
     reply: 'Cùng nhau cố gắng nhaaaaa <333 ',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Tao biết mà 🥰.  cảm ơn mày vì đã đến bên tao 😘😘',
     messDesc: 'Nhắn tin tao nè  .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/Quang090203/' //link mess của các bạn. 
